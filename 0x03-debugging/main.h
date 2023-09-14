b7957cbff76cc19c46ab3bb4b2ffd2cc7e58dd56
Balamathias <balamathias05@gmail.com>
2023-09-14 07:16:20 -0700

    changed main.h file

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,7 +3,11 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int i);
+void positive_or_negative(int i) {
+	if (i == 0) {printf("%d is zero", i)};
+	if (i < 0) {printf("%d is negative", i)};
+	if (i > 0) {printf("%d is positive", i)};
+}
 
 #endif /* MAIN_H */
 
