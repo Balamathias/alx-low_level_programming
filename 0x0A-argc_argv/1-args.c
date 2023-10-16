d2295c60331da2a2aae55097d32ea51a8046cd9d
Balamathias <balamathias05@gmail.com>
2023-10-15 21:24:43 -0700

    My new commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main-Prints the name
+ * @argc: The argument count
+ * @argv: The array of inputs
+ * Return: Always 0 when successful, 1 when it's not
+ */
+
+int main(int argc, char *argv[])
+{
+        (void) argv;
+        printf("%i\n", argc-1);
+        
+        return (0);
+}
