f58f1aa7f1e49929c998ef49a979fe600260b168
Balamathias <balamathias05@gmail.com>
2023-10-15 21:53:27 -0700

    yet another

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main-Prints the name
+ * @argc: The argument count
+ * @argv: The array of inputs
+ * Return: Always 0 when successful, 1 when it's not
+ */
+
+int main(int argc, char *argv[])
+{
+        int n1, n2;
+	if (argc == 3){
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+        	printf("%i\n", n1 * n2);
+	} else {
+		printf("Error\n");
+		return (1);
+	}
+	return (0)
+}
