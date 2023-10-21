7211797ac22d0d9cb6ab10e7adde1cd967f13954
Balamathias <balamathias05@gmail.com>
2023-10-21 12:29:35 -0700

    New commit

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,22 +1,10 @@
-section .data
-    hello db 'Hello Hoolerton',0
-    hello_len equ $ - hello
-
-section .text
-    global _start
-
-_start:
-    ; Prepare the parameters for the write system call
-    mov rax, 1         ; syscall number for sys_write
-    mov rdi, 1         ; file descriptor (1 = stdout)
-    mov rsi, hello     ; pointer to the string to be printed
-    mov rdx, hello_len ; length of the string
-
-    ; Make the system call
-    syscall
-
-    ; Exit the program
-    mov rax, 60        ; syscall number for sys_exit
-    xor rdi, rdi       ; exit status
-    syscall
+global    main
+          extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
 
